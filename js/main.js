cc24b72a9bfab21a7837e03537acd05414648a42
StanislavTihanov <117075179+StanislavTihanov@users.noreply.github.com>
2025-02-15 15:50:30 +0300

    Создал секцию Последние статьи

--- js/main.js
+++ js/main.js
@@ -148,7 +148,76 @@ if (mainSlider) {
     },
   });
 }
-
+const categoriesSliders = document.querySelectorAll('.categories-slider');
+categoriesSliders.forEach((slider, index) => {
+  new Swiper(slider, {
+    direction: 'horizontal',
+    spaceBetween: 20,
+    speed: 1000,
+    autoHeight: false,
+    navigation: {
+      nextEl: `.swiper-button-next-${index}`,
+      prevEl: `.swiper-button-prev-${index}`,
+    },
+    breakpoints: {
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    }
+  });
+});
+const cesSliders = document.querySelectorAll('.sec-slider');
+cesSliders.forEach((slider, index) => {
+  new Swiper(slider, {
+    direction: 'horizontal',
+    spaceBetween: 40,
+    speed: 1000,
+    autoHeight: false,
+    loop: true,
+    navigation: {
+      nextEl: `.swiper-button-next-${index}`,
+      prevEl: `.swiper-button-prev-${index}`,
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1.8,  // Например, для экранов шириной до 480px будет 1 слайд
+        spaceBetween: 10,  // Интервал между слайдами уменьшится
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 6,
+        spaceBetween: 30,  // Можно изменить интервал между слайдами на 30px для разрешения 1024px
+      },
+    }
+  });
+});
+const partnersSlider = document.querySelector('.partners-slider');
+if (partnersSlider) {
+  new Swiper(partnersSlider, {
+    direction: 'horizontal',
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 40,
+    speed: 2000,
+    autoHeight: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-prev',
+      prevEl: '.swiper-button-next',
+    },
+  });
+}
 //------------------------------------------------------------------------Слайдер
 
 
@@ -178,29 +247,24 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 //-----------------------------------------------------------------------код для кнопок показать скрыть текст
 
-const categoriesSliders = document.querySelectorAll('.categories-slider');
-categoriesSliders.forEach((slider, index) => {
-  new Swiper(slider, {
-    direction: 'horizontal',
-    spaceBetween: 20,
-    speed: 1000,
-    autoHeight: false,
-    navigation: {
-      nextEl: `.swiper-button-next-${index}`,
-      prevEl: `.swiper-button-prev-${index}`,
-    },
-    breakpoints: {
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-      },
-      1024: {
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
-    }
-  });
+
+
+document.getElementById("fileInput").addEventListener("change", function() {
+  const fileName = this.files.length > 0 ? this.files[0].name : "Файл не выбран";
+  document.getElementById("fileName").textContent = fileName;
 });
+
+
+//------------------------------------------------------------------------Fancybox
+document.addEventListener("DOMContentLoaded", function () {
+  if (typeof Fancybox !== "undefined" && typeof Fancybox.bind === "function") {
+      Fancybox.bind("[data-fancybox]", {
+          // Кастомные опции
+      });
+  }
+});
+//------------------------------------------------------------------------Fancybox
+
 
 //------------------------------------------------------------------------select выпадающий список
 //document.querySelectorAll('.dropdown').forEach(function(dropDownWrapper) {
@@ -279,104 +343,104 @@ categoriesSliders.forEach((slider, index) => {
 
 
 //------------------------------------------------------------------------popup
-//const popupLinks = document.querySelectorAll('.popup-link');
-//const body = document.querySelector('body');
-//const lockPadding = document.querySelectorAll(".lock-padding");
-//
-//
-//let unlock = true;
-//
-//const timeout = 800;
-//
-//if (popupLinks.length > 0) {
-//  for (let index = 0; index < popupLinks.length; index++) {
-//    const popupLink = popupLinks[index];
-//    popupLink.addEventListener("click", function (e) {
-//      const popupName = popupLink.getAttribute('href').replace('#', '');
-//      const currentPopup = document.getElementById(popupName);
-//      popupOpen(currentPopup);
-//      e.preventDefault();
-//    });
-//  }
-//}
-//
-//const popupCloseIcon = document.querySelectorAll('.close-popup');
-//if (popupCloseIcon.length > 0) {
-//  for (let index = 0; index < popupCloseIcon.length; index++) {
-//    const el = popupCloseIcon[index];
-//    el.addEventListener('click', function (e) {
-//      popupClose(el.closest('.popup'));
-//      e.preventDefault();
-//    })
-//  }
-//}
-//
-//function popupOpen(currentPopup) {
-//  if (currentPopup && unlock) {
-//    const popupActive = document.querySelector('.popup.open');
-//    if (popupActive) {
-//      popupClose(popupActive, false);
-//    } else {
-//      bodyLock();
-//    }
-//    currentPopup.classList.add('open');
-//    currentPopup.addEventListener("click", function (e) {
-//      if (!e.target.closest('.popup__content')) {
-//        popupClose(e.target.closest('.popup'));
-//      }
-//    });
-//  }
-//}
-//
-//function popupClose(popupActive, doUnlock = true) {
-//  if (unlock) {
-//    popupActive.classList.remove('open');
-//    if (doUnlock) {
-//      bodyUnlock();
-//    }
-//  }
-//}
-//
-//function bodyLock() {
-//  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
-//  if (lockPadding.length > 0) {
-//    for (let index = 0; index < lockPadding.length; index++) {
-//      const el = lockPadding[index];
-//      el.style.paddingRight = lockPaddingValue;
-//    }
-//  }
-//  body.style.paddingRight = lockPaddingValue;
-//  body.classList.add('lock');
-//
-//  unlock = false;
-//  setTimeout(function () {
-//    unlock = true;
-//  }, timeout);
-//}
-//
-//function bodyUnlock () {
-//  setTimeout(function () {
-//    if(lockPadding.length > 0) {
-//      for (let index = 0; index < lockPadding.length; index++) {
-//        const el = lockPadding[index];
-//        el.style.paddingRight = '0px';
-//      }
-//  }
-//    body.style.paddingRight = '0px';
-//    body.classList.remove('lock');
-//  }, timeout);
-//  unlock = false;
-//  setTimeout(function () {
-//    unlock = true;
-//  }, timeout);
-//}
-//
-//document.addEventListener('keydown', function (e) {
-//  if (e.which === 27) {
-//    const popupActive = document.querySelector('.popup.open');
-//    popupClose(popupActive);
-//  }
-//});
+const popupLinks = document.querySelectorAll('.popup-link');
+const body = document.querySelector('body');
+const lockPadding = document.querySelectorAll(".lock-padding");
+
+
+let unlock = true;
+
+const timeout = 800;
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index];
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute('href').replace('#', '');
+      const currentPopup = document.getElementById(popupName);
+      popupOpen(currentPopup);
+      e.preventDefault();
+    });
+  }
+}
+
+const popupCloseIcon = document.querySelectorAll('.close-popup');
+if (popupCloseIcon.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index];
+    el.addEventListener('click', function (e) {
+      popupClose(el.closest('.popup'));
+      e.preventDefault();
+    })
+  }
+}
+
+function popupOpen(currentPopup) {
+  if (currentPopup && unlock) {
+    const popupActive = document.querySelector('.popup.open');
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add('open');
+    currentPopup.addEventListener("click", function (e) {
+      if (!e.target.closest('.popup__content')) {
+        popupClose(e.target.closest('.popup'));
+      }
+    });
+  }
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  if (unlock) {
+    popupActive.classList.remove('open');
+    if (doUnlock) {
+      bodyUnlock();
+    }
+  }
+}
+
+function bodyLock() {
+  const lockPaddingValue = window.innerWidth - document.querySelector('.wrapper').offsetWidth + 'px';
+  if (lockPadding.length > 0) {
+    for (let index = 0; index < lockPadding.length; index++) {
+      const el = lockPadding[index];
+      el.style.paddingRight = lockPaddingValue;
+    }
+  }
+  body.style.paddingRight = lockPaddingValue;
+  body.classList.add('lock');
+
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+function bodyUnlock () {
+  setTimeout(function () {
+    if(lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight = '0px';
+      }
+  }
+    body.style.paddingRight = '0px';
+    body.classList.remove('lock');
+  }, timeout);
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, timeout);
+}
+
+document.addEventListener('keydown', function (e) {
+  if (e.which === 27) {
+    const popupActive = document.querySelector('.popup.open');
+    popupClose(popupActive);
+  }
+});
 //------------------------------------------------------------------------popup
 
 
